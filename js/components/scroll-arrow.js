5edd7f3ae58b3ad2eb39e1649a65f824eb533331
seiya.ie <seiya2342@icloud.com>
2025-09-19 12:21:43 +0900

    FV用スクロール矢印の表示、非表示制御を実装しました。

---
+++ js/components/scroll-arrow.js
@@ -0,0 +1,40 @@
+export const initScrollArrow = () => {
+    const indicator = document.querySelector(".js-indicator");
+    const arrow = document.querySelector(".js-arrow");
+
+    gsap.fromTo(
+        arrow,
+        {
+            y: -20,
+        },
+        {
+            y: 20,
+            duration: 1.2,
+            repeat: -1,
+            ease: "power.in",
+        }
+    );
+
+    ScrollTrigger.create({
+        trigger: indicator,
+        scroller: "body",
+        start: "top top",
+        markers: true,
+        onEnter: () =>
+            gsap.to(indicator, {
+                autoAlpha: 0,
+                y: 16,
+                duration: 0.4,
+                ease: "power2.out",
+                overwrite: "auto",
+            }),
+        onLeaveBack: () =>
+            gsap.to(indicator, {
+                autoAlpha: 1,
+                y: 0,
+                duration: 0.4,
+                ease: "power2.out",
+                overwrite: "auto",
+            }),
+    });
+};
