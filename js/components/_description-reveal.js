b80b4ed6e04d83dbf9cc18eb234cfc93949e92f5
seiya.ie <seiya2342@icloud.com>
2025-09-26 09:02:01 +0900

    description-reveal.js を作成

---
+++ js/components/_description-reveal.js
@@ -0,0 +1,16 @@
+export const initDescReveal = () => {
+    const description = document.querySelectorAll(".js-description-item");
+
+    description.forEach((item) => {
+        gsap.from(item, {
+            opacity: 0,
+            y: 24,
+            duration: 0.7,
+            ease: "power4.out",
+            scrollTrigger: {
+                trigger: item,
+                start: "top 70%",
+            },
+        });
+    });
+};
